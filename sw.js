5bb281a73092fc90ac92641bb979198b1ece6d6c
may1as <94220731+may1as@users.noreply.github.com>
2023-04-15 20:28:05 +0800

    1
    1

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230415.201950';
+const cacheName = 'chirpy-20230415.202802';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
